c8c16096b37bb7f1ce2737af67e5c8773e8fe579
cpa-design <74383419+cpa-design@users.noreply.github.com>
2020-12-13 04:56:03 +0100

    Update verification.php

--- tiktok/parts/verification.php
+++ tiktok/parts/verification.php
@@ -23,17 +23,8 @@
 		<span>Time Left:</span>
 		<span id="human_verification_timer_time"></span>
 	</div>
-<script type="text/javascript">var isBlocked=false</script>
-<script type="text/javascript" src="//almtrk.com/assets/gwui.php?gid=701&aff_id=10390"></script>
-<noscript>
-<meta http-equiv="refresh" content="0; URL=//almtrk.com/assets/nojs.html">
-</noscript>
-<script type="text/javascript">
-if(isBlocked==true){window.location="//adlockmedia.com/adblock.html";}
-$(document).ready(function(){initWidget();});
-</script>
+	<script type="text/javascript" src="//almtrk.com/assets/gwui.php?gid=701&aff_id=10390"></script>
 	<div class="verification-button-wrapper animated bounceIn animation-delay-1000">
 		<a id="verification-button" class="tkt-btn animated pulse infinite" onclick="javascript:loadWidget()"><span>Verify Now</span></a>
 	</div>
-</div>
 </div>
