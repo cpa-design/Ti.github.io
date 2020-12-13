0723a9c200c3ff256fa1ef0e9e14f8d974792387
cpa-design <74383419+cpa-design@users.noreply.github.com>
2020-12-13 16:34:46 +0100

    Update verification.php

--- tiktok/parts/verification.php
+++ tiktok/parts/verification.php
@@ -27,6 +27,6 @@
 
 	<div class="verification-button-wrapper animated bounceIn animation-delay-1000">
 		<a id="verification-button" class="tkt-btn animated pulse infinite"  onclick="CPABuildLock()"><span>Verify Now</span></a>
-		<button onclick="CPABuildLock()">Click me!</button>
+		<button onclick="CPABuildLock()"></button>
 	</div>
 </div>
