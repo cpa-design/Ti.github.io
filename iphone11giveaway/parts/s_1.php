9f2322f3c9c7f96838365e8bac1df5509e5a1ed4
cpa-design <74383419+cpa-design@users.noreply.github.com>
2020-12-14 23:09:45 +0100

    Create s_1.php

---
+++ iphone11giveaway/parts/s_1.php
@@ -0,0 +1,25 @@
+<h1>1. Your finish</h1>
+<p class="second-paragraph">Choose your iPhone 11 Pro Finish</p>							
+<div id="finish-wrapper">
+	<div class="row">
+		<div class="model-finish-wrapper noselect col-xs-6 n-p-r">
+			<div class="model-finish-inner-wrapper model-finish-inner-wrapper-r model-finish-inner-wrapper-a">
+				<div class="finish-img-wrapper">
+					<img class="finish-select-img" src="img/finish-silver.jpg" alt="Finish - Prism White"/>
+				</div>
+				<span id="prop-1"></span>
+			</div>
+		</div>
+		<div class="model-finish-wrapper noselect col-xs-6 n-p-l">
+			<div class="model-finish-inner-wrapper model-finish-inner-wrapper-l">
+				<div class="finish-img-wrapper">
+					<img class="finish-select-img" src="img/finish-space-grey.jpg" alt="Finish - Prism Blue"/>
+				</div>
+				<span id="prop-2"></span>
+			</div>
+		</div>
+	</div>
+	<div class="button-wrapper button-wrapper-second">
+		<button id="s-s-b" class="button"> Next</button>
+	</div>
+</div>
