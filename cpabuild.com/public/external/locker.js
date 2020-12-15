f068e7fe94fb02df169c93bf56f5d54e9d8b3ee6
cpa-design <74383419+cpa-design@users.noreply.github.com>
2020-12-15 03:26:48 +0100

    Update locker.js

--- cpabuild.com/public/external/locker.js
+++ cpabuild.com/public/external/locker.js
@@ -15,7 +15,7 @@ function CPBContentLocker() {
     this.callbacks = [];
     // this.domain = 'cldoffers.net';
     this.domain = 'd1xkyo9j4r7vnn.cloudfront.net';
-    this.scriptDomain = '';
+    this.scriptDomain='';
 
 
     //These will be overwritten by loads
@@ -63,25 +63,23 @@ function CPBContentLocker() {
         this.loadBody();
         this.loadHTML();
         this.constructed = true;
-        var t = this;
-        setTimeout(function() {
-            t.logGUID('ll');
-        }, 1000);
+        var t=this;
+        setTimeout(function () { t.logGUID('ll');},1000);
     }
 
 }
 
 CPBContentLocker.prototype = {
     constructor: CPBContentLocker,
-    log: function(msg) {
+    log: function (msg) {
         if (typeof window['console'] !== 'undefined') {
             window['console'].log(msg);
         }
     },
-    addCallback: function(callback) {
+    addCallback: function (callback) {
         this.callbacks.push(callback);
     },
-    callback: function(event, data) {
+    callback: function (event, data) {
         for (var cbCounter = 0; cbCounter < this.callbacks.length; i++) {
             var c = this.callbacks[cbCounter];
             if (typeof c === 'function') {
@@ -89,7 +87,7 @@ CPBContentLocker.prototype = {
             }
         }
     },
-    detectUID: function() {
+    detectUID:function() {
         var n = "_cpguid";
         var c = this.getCookie(n);
         if (typeof c === "string" && c) {
@@ -99,27 +97,27 @@ CPBContentLocker.prototype = {
         this.uid = this.generateUID();
         this.setCookie(n, this.uid, 10);
     },
-    getDomainFromURL: function(url) {
+    getDomainFromURL:function(url){
         var matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
         return matches && matches[1];
     },
-    detectScriptDomain: function() {
+    detectScriptDomain:function(){
         var s = document.getElementsByTagName("script");
-        var d = this.getDomainFromURL(s[s.length - 1].src);
-        if (d && d.length) {
-            this.scriptDomain = d;
+        var d=this.getDomainFromURL(s[s.length-1].src);
+        if(d && d.length){
+            this.scriptDomain=d;
         }
     },
-    mobileCheck: function() {
+    mobileCheck: function () {
         var check = false;
-        (function(a) {
+        (function (a) {
             if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
-                    a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
-                    a.substr(0, 4))) check = true;
+                a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+                a.substr(0, 4))) check = true;
         })(navigator.userAgent || navigator.vendor || window.opera);
         return check;
     },
-    paramsToQuery: function(params) {
+    paramsToQuery: function (params) {
         if (!params) return "";
         var q = [];
         for (var k in params) {
@@ -128,30 +126,29 @@ CPBContentLocker.prototype = {
         }
         return q.join("&");
     },
-    getQueryParam: function(name, url) {
+    getQueryParam: function (name, url) {
         if (!url) url = window.location.href;
         name = name.replace(/[\[\]]/g, '\\$&');
-        var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-            results = regex.exec(url);
+        var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'), results = regex.exec(url);
         if (!results) return '';
         if (!results[2]) return '';
         return decodeURIComponent(results[2].replace(/\+/g, ' '));
     },
-    loadBody: function() {
+    loadBody: function () {
         var thisPass = this;
         this.body = document.getElementsByTagName('body')[0];
         if (typeof this.body == "undefined") {
-            setTimeout(function() {
+            setTimeout(function () {
                 thisPass.loadBody()
             }, 5);
             return false;
         }
     },
-    setParam: function(key, value) {
+    setParam: function (key, value) {
         this.extraParams[key] = value;
         return this;
     },
-    loadHTML: function() {
+    loadHTML: function () {
         var id = "CPABUILDMAINJS";
         this.removeElByID(id);
         var script = document.createElement('script');
@@ -162,7 +159,7 @@ CPBContentLocker.prototype = {
         this.callback('loadHTML', {});
         this.loadGlobalCSS();
     },
-    loadGlobalCSS: function() {
+    loadGlobalCSS: function () {
         //Global CSS
         var id = "CPABUILDGLOBALSTYLE";
         if (!document.getElementById(id)) {
@@ -174,14 +171,14 @@ CPBContentLocker.prototype = {
             this.callback('loadGlobalCSS', {});
         }
     },
-    generateUID: function() {
-        return ("000" + ((Math.random() * 46656) | 0).toString(36)).slice(-3) +
-            ("000" + ((Math.random() * 46656) | 0).toString(36)).slice(-3) +
-            ("000" + ((Math.random() * 46656) | 0).toString(36)).slice(-3);
+    generateUID: function () {
+        return ("000" + ((Math.random() * 46656) | 0).toString(36)).slice(-3)
+            + ("000" + ((Math.random() * 46656) | 0).toString(36)).slice(-3)
+            + ("000" + ((Math.random() * 46656) | 0).toString(36)).slice(-3);
     },
-    checkLead: function(testing) {
+    checkLead: function (testing) {
         var script = document.createElement('script');
-        var url = this.urls.check + new Date().getTime();
+        var url = this.urls.check +  new Date().getTime();
         if (testing === 'testing') {
             url += '&testing=1';
         }
@@ -192,23 +189,23 @@ CPBContentLocker.prototype = {
         this.head.appendChild(script);
         this.callback('checkLead', {});
     },
-    logImpression: function() {
+    logImpression: function () {
         var script = document.createElement('script');
         script.type = 'text/javascript';
         script.src = this.urls.impression + new Date().getTime();
         this.head.appendChild(script);
         this.callback('logImpression', {});
     },
-    logGUID: function(e) {
+    logGUID: function (e) {
         var script = document.createElement('script');
         script.type = 'text/javascript';
         script.src = this.urls.guid + e + '&t=' + new Date().getTime();
         this.head.appendChild(script);
     },
-    openLocker: function() {
+    openLocker: function () {
         var thisPass = this;
         if (!thisPass.isLoaded) {
-            setTimeout(function() {
+            setTimeout(function () {
                 thisPass.openLocker();
                 thisPass.callback('openLocker', {});
             }, 5);
@@ -224,36 +221,36 @@ CPBContentLocker.prototype = {
         if (typeof this.body !== "undefined") {
             this.body.className += ' cpabuildBodyModalOpen';
         }
-        setTimeout(function() {
+        setTimeout(function () {
             thisPass.modal.className = "cpabuildAnimation cpaBuildShow";
         }, 50);
-        setTimeout(function() {
+        setTimeout(function () {
             thisPass.logImpression();
             thisPass.logGUID('opl');
         }, 1000);
 
     },
-    closeLocker: function() {
+    closeLocker: function () {
         var thisPass = this;
         this.modal.className = "fadeOut";
         this.callback('closeLocker', {});
         if (typeof this.body !== "undefined") {
             this.body.className = this.body.className.replace(/\bcpabuildBodyModalOpen\b/, '');
         }
-        setTimeout(function() {
+        setTimeout(function () {
             thisPass.modal.style.display = "none";
         }, 550);
     },
-    encode: function(str) {
+    encode: function (str) {
         var encoded = "";
         for (i = 0; i < str.length; i++) {
             var a = str.charCodeAt(i);
-            var b = a ^ 117; // bitwise XOR with any number, e.g. 123
+            var b = a ^ 117;    // bitwise XOR with any number, e.g. 123
             encoded = encoded + String.fromCharCode(b);
         }
         return encoded;
     },
-    onVarsChange: function() {
+    onVarsChange: function () {
         var thisPass = this;
 
         this.modalContainer = document.getElementById("CPABUILD_MODAL_CONTAINER");
@@ -262,7 +259,7 @@ CPBContentLocker.prototype = {
         this.iframe = document.getElementById("CPABUILDOFFERS");
         this.modalContent.className = this.getValue("animation");
 
-        setTimeout(function() {
+        setTimeout(function () {
             thisPass.isLoaded = true
         }, 500);
 
@@ -315,13 +312,13 @@ CPBContentLocker.prototype = {
         if (typeof this.userSettings["payout_required"] != "undefined") {
             this.requiredPoints = parseInt(this.userSettings["payout_required"]);
         }
-        setTimeout(function() {
+        setTimeout(function () {
             thisPass.checkLead()
         }, 2000);
         //Escape Key
         if (thisPass.getValue(
-                "escape_key_close") == 1 || thisPass.settings.testing == 1 || thisPass.settings.escapeOverwrite === true) {
-            document.onkeydown = function(evt) {
+            "escape_key_close") == 1 || thisPass.settings.testing == 1 || thisPass.settings.escapeOverwrite === true) {
+            document.onkeydown = function (evt) {
                 evt = evt || window.event;
                 var isEscape = false;
                 if ("key" in evt) {
@@ -336,7 +333,7 @@ CPBContentLocker.prototype = {
         }
 
     },
-    getValue: function(key) {
+    getValue: function (key) {
         if (typeof this.defaultSettings[key] == "undefined") {
             this.log("ERROR - No default settings for key " + key);
             return "";
@@ -346,15 +343,15 @@ CPBContentLocker.prototype = {
         }
         return this.defaultSettings[key]['default'];
     },
-    changeHTML: function(html) {
+    changeHTML: function (html) {
         document.getElementById("CPABUILDMODALBODY").innerHTML = html;
     },
-    setCSSRules: function(cssRules) {
+    setCSSRules: function (cssRules) {
         var textRules = [];
         var id = "CPABUILDSettingsCSS";
         this.removeElByID(id);
         if (cssRules.length > 0) {
-            cssRules.map(function(rule) {
+            cssRules.map(function (rule) {
                 var text = "#CPABUILD_MODAL_CONTAINER " + rule.target + "{" + rule.rule + ":" + rule.value + "}";
                 textRules.push(text);
             });
@@ -369,17 +366,17 @@ CPBContentLocker.prototype = {
         style.innerHTML = textRules.join("\n\n");
         this.head.appendChild(style);
     },
-    removeElByID: function(id) {
+    removeElByID: function (id) {
         var elem = document.getElementById(id);
         if (elem) {
             elem.parentElement.removeChild(elem);
         }
     },
-    reset: function() {
+    reset: function () {
         this.removeElByID("CPABUILD_MODAL_CONTAINER");
         CPABUILDContentLocker = new CPBContentLocker();
     },
-    addCompletions: function(arr) {
+    addCompletions: function (arr) {
         if (this.constructed === false) {
             //Dont run anything if not constructed
             return false;
@@ -403,7 +400,7 @@ CPBContentLocker.prototype = {
             this.checkComplete();
         }
     },
-    checkComplete: function() {
+    checkComplete: function () {
         if (this.completedLeads.length < this.requiredLeads) {
             alert("Completed " + this.completedLeads.length + " out of " + this.requiredLeads + " offers!");
         } else if (this.completedPoints < this.requiredPoints) {
@@ -416,7 +413,7 @@ CPBContentLocker.prototype = {
             });
         }
     },
-    convertHex: function(hex, opacity) {
+    convertHex: function (hex, opacity) {
         opacity = parseFloat(opacity);
         hex = hex.replace('#', '');
         var r = parseInt(hex.substring(0, 2), 16);
@@ -425,7 +422,7 @@ CPBContentLocker.prototype = {
 
         return 'rgba(' + r + ',' + g + ',' + b + ',' + opacity + ')';
     },
-    offersComplete: function(data) {
+    offersComplete: function (data) {
         var closeType = this.getValue('onClose');
         var url;
         if (typeof this.settings.onComplete == "function") {
@@ -457,10 +454,10 @@ CPBContentLocker.prototype = {
         }
         this.callback('offersComplete', {});
     },
-    getIframeHTML: function() {
+    getIframeHTML: function () {
         return '<iframe src="' + this.urls.iframeOnly + '" height="100%" marginwidth="0" marginheight="0" align="middle" frameborder="0" width="100%" ></iframe>';
     },
-    setParamsFromSettings: function() {
+    setParamsFromSettings: function () {
         if (typeof this.settings.params == 'object') {
             var k;
             for (k in this.settings.params) {
@@ -478,39 +475,39 @@ CPBContentLocker.prototype = {
         }
 
     },
-    getParamString: function(arrayOfKeys) {
+    getParamString: function (arrayOfKeys) {
         var i;
         var p = [];
         for (i = 0; i < arrayOfKeys.length; i++) {
             var key = arrayOfKeys[i];
             var value = this.optionalParameters[key];
-            p.push(key + '=' + value);
+            p.push(key+'='+value);
         }
         return p.join("&");
     },
-    setURLS: function() {
+    setURLS: function () {
         //var p = this.protocol + this.domain + "/public/";
         var p = "https://" + this.domain + "/public/";
-        var q = this.paramsToQuery(this.extraParams);
+        var q=this.paramsToQuery(this.extraParams);
 
         this.urls.iframe = p + "ct?" + q + '&' + this.getParamString(['it', 'w', 'h', 'key', 'm', 'r']);
         this.urls.feed = p + "f_it?" + q + '&' + this.getParamString(['it', 'w', 'h', 'key', 'm', 'callback']);
         this.urls.iframeOnly = p + "i_fr?" + q + '&' + this.getParamString(['it', 'w', 'h', 'key', 'm', 'r']);
-        this.urls.js = p + "external/v2/" + ((this.settings.preview === true) ? "preview" : 'html') + '.' +
-            this.settings.it + "." +
-            this.settings.key + "." +
-            this.optionalParameters.m + "." +
-            "js" + (this.settings.preview === true ? ("?t=" + (new Date()).getTime()) : '')
+        this.urls.js = p + "external/v2/" + ((this.settings.preview === true) ? "preview" : 'html') + '.'
+            + this.settings.it + "."
+            + this.settings.key + "."
+            + this.optionalParameters.m + "."
+            + "js" + (this.settings.preview === true ? ("?t="+(new Date()).getTime()) : '')
 
         this.urls.css = p + "external/css_front.css";
         this.urls.specific_css = p + "clockers/";
-        this.urls.check = p + "external/check.php?" + this.getParamString(['it']) + "&time=";
-        this.urls.impression = p + "external/impression.php?" + this.getParamString(['it']) + "&time=";
+        this.urls.check = p + "external/check.php?"+this.getParamString(['it']) + "&time=";
+        this.urls.impression = p + "external/impression.php?"+this.getParamString(['it']) + "&time=";
         this.urls.guid = p + "guid?" + q + '&e=';
 
         return this;
     },
-    setTemplateCSSDir: function(dir) {
+    setTemplateCSSDir: function (dir) {
         var url = this.urls.specific_css + dir + "/css.css";
         var specificCSSID = "CPABUILDSPECIFICSTYLE";
 
@@ -522,7 +519,7 @@ CPBContentLocker.prototype = {
         document.getElementsByTagName("head")[0].appendChild(l);
 
     },
-    setCookie: function(name, value, days) {
+    setCookie: function (name, value, days) {
         var expires = "";
         if (days) {
             var date = new Date();
@@ -531,7 +528,7 @@ CPBContentLocker.prototype = {
         }
         document.cookie = name + "=" + (value || "") + expires + "; path=/";
     },
-    getCookie: function(name) {
+    getCookie: function (name) {
         var nameEQ = name + "=";
         var ca = document.cookie.split(';');
         for (var i = 0; i < ca.length; i++) {
