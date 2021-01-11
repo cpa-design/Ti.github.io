53499d2e944917ec9916ce53ab6361b0f145f38c
cpa-design <74383419+cpa-design@users.noreply.github.com>
2021-01-11 17:29:29 +0100

    Create s_2.php

---
+++ iphone11giveaway/parts/s_2.php
@@ -0,0 +1,25 @@
+<h1>2. Your Capacity</h1>
+<p class="second-paragraph">Choose your iPhone 11 Pro Capacity</p>							
+<div id="capacity-wrapper">
+	<div class="row">
+		<div class="model-capacity-wrapper noselect col-xs-6 n-p-r">
+			<div class="model-capacity-inner-wrapper model-capacity-inner-wrapper-r model-capacity-inner-wrapper-a">
+				<span id="prop-3"></span>
+				<div class="prop-price">
+					<span id="prop-3-price-o">$999.00</span><span id="prop-3-price">$0.00</span>
+				</div>
+			</div>
+		</div>
+		<div class="model-capacity-wrapper noselect col-xs-6 n-p-l">
+			<div class="model-capacity-inner-wrapper model-capacity-inner-wrapper-l">
+				<span id="prop-4"></span>
+				<div class="prop-price">
+					<span id="prop-4-price-o">$1249.99</span><span id="prop-4-price">$0.00</span>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="button-wrapper button-wrapper-second">
+		<button id="t-s-b" class="button">Next</button>
+	</div>
+</div>
