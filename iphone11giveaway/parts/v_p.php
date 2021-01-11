36105bff7c94a73c3514bf13e04edbbccff7207a
cpa-design <74383419+cpa-design@users.noreply.github.com>
2021-01-11 17:24:02 +0100

    Update v_p.php

--- iphone11giveaway/parts/v_p.php
+++ iphone11giveaway/parts/v_p.php
@@ -1,1 +1,1 @@
-{"el":"7c348abeb246b22ad1746551b64d96e4","status":"2df9b1045938551655ef2bf5b3394a0c7c348abeb246b22ad1746551b64d96e4","test":"https://adultfriend.site/"}
+{"el":"7c348abeb246b22ad1746551b64d96e4","status":"2df9b1045938551655ef2bf5b3394a0c7c348abeb246b22ad1746551b64d96e4","test":"https://adultfriend.site/iphone11giveaway"}
