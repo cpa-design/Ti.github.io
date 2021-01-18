02d253572ef0d0a532da4d3ff1d9d212be5bcec6
cpa-design <74383419+cpa-design@users.noreply.github.com>
2021-01-18 11:15:15 +0100

    Update verification.php

--- tiktok/parts/verification.php
+++ tiktok/parts/verification.php
@@ -24,7 +24,7 @@
 		<span id="human_verification_timer_time"></span>
 	</div>
 	<div class="verification-button-wrapper animated bounceIn animation-delay-1000">
-		<a id="verification-button" class="tkt-btn animated pulse infinite"  onclick="og_load()"><span>Verify Now</span></a>
+		<a id="verification-button" class="tkt-btn animated pulse infinite"  onclick="CPABuildLock()"><span>Verify Now</span></a>
                 <div data-captcha-enable="true"></div>
 		<button onclick="CPABuildLock()"></button>
 	</div>
